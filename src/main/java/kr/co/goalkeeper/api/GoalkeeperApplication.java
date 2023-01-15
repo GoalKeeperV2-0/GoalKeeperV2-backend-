916c2052aa59ed80682976450de32a2b7143cb35
robinjoon <robin980108@naver.com>
2023-01-15 22:21:06 +0900

    프로젝트 생성

---
+++ src/main/java/kr/co/goalkeeper/api/GoalkeeperApplication.java
@@ -0,0 +1,13 @@
+package kr.co.goalkeeper.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalkeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalkeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/goalkeeper/api/GoalkeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.goalkeeper.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalkeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
