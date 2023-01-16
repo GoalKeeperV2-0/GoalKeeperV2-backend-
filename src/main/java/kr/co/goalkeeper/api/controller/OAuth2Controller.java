73c4dc896fd21cc2590a9dbef6d04ee66eb42d4e
robinjoon <robin980108@naver.com>
2023-01-17 01:22:19 +0900

    OAuth2Controller 메서드 정리

--- src/main/java/kr/co/goalkeeper/api/controller/OAuth2Controller.java
+++ src/main/java/kr/co/goalkeeper/api/controller/OAuth2Controller.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 import java.util.Map;
 @RestController
 @RequestMapping("api/oauth2/")
@@ -27,10 +29,32 @@ public class OAuth2Controller {
 
     @GetMapping("/{snsType}")
     public ResponseEntity<?> oauth(@PathVariable("snsType")OAuthType oAuthType, @RequestParam String code){
-        OAuthAccessToken authAccessToken = googleOAuth2Service.getAccessToken(code);
-
-        Map<String, Object> credential = googleOAuth2Service.getCredential(authAccessToken);
+        Map<String, Object> credential;
+        switch (oAuthType){
+            case GOOGLE:
+                credential = googleOAuth(code);
+                break;
+            case KAKAO:
+                credential = kakaoOAuth(code);
+                break;
+            case NAVER:
+                credential = naverOAuth(code);
+                break;
+            default:
+                throw new IllegalArgumentException("지원하지 않는 방식 입니다.");
+        }
         log.info(credential.toString());
         return ResponseEntity.ok(credential);
+    }
+    private Map<String, Object> googleOAuth(String code){
+        OAuthAccessToken authAccessToken = googleOAuth2Service.getAccessToken(code);
+
+        return googleOAuth2Service.getCredential(authAccessToken);
+    }
+    private Map<String, Object> naverOAuth(String code){
+        return new HashMap<>();
+    }
+    private Map<String, Object> kakaoOAuth(String code){
+        return new HashMap<>();
     }
 }
