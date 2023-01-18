153e56181f222184dc5308de4b7192f4f12431de
robinjoon <robin980108@naver.com>
2023-01-18 21:41:26 +0900

    CORS 설정 적용

--- src/main/java/kr/co/goalkeeper/api/config/spring/WebConfig.java
+++ src/main/java/kr/co/goalkeeper/api/config/spring/WebConfig.java
@@ -2,6 +2,7 @@ package kr.co.goalkeeper.api.config.spring;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -9,5 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new OAuthTypeConverter());
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*");
     }
 }
