c5ae2f72fe874cb3ee881ff92f82ab412c9f3205
robinjoon <robin980108@naver.com>
2023-01-20 23:26:24 +0900

    응답 헤더 로그 오류 수정

--- src/main/java/kr/co/goalkeeper/api/log/APIResponseLogger.java
+++ src/main/java/kr/co/goalkeeper/api/log/APIResponseLogger.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Enumeration;
 
 @Slf4j
@@ -18,21 +20,11 @@ public class APIResponseLogger implements HandlerInterceptor {
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        String requestURI = request.getRequestURI();
-        requestURI = URLDecoder.decode(requestURI, StandardCharsets.UTF_8);
         if (request.getClass().getName().contains("SecurityContextHolderAwareRequestWrapper")) return;
         final ContentCachingRequestWrapper cachingRequest = (ContentCachingRequestWrapper) request;
         final ContentCachingResponseWrapper cachingResponse = (ContentCachingResponseWrapper) response;
         String uuid = (String) cachingRequest.getAttribute("uuid");
         String type="Response ===> uuid = ";
-        Enumeration<String> headerNames = request.getHeaderNames();
-        log.info("{}{} {}",type,uuid,"headers start");
-        while (headerNames.hasMoreElements()){
-            String headerName = headerNames.nextElement();
-            String header = request.getHeader(headerName);
-            log.info("{}{} {}: {}",type,uuid,headerName,header);
-        }
-        log.info("{}{} {}",type,uuid,"headers end");
         if(ex==null) {
             log.info("{}{}, Response Body : {}",type, uuid, objectMapper.readTree(cachingResponse.getContentAsByteArray()));
         }else {
--- src/main/java/kr/co/goalkeeper/api/log/CustomServletWrappingFilter.java
+++ src/main/java/kr/co/goalkeeper/api/log/CustomServletWrappingFilter.java
@@ -1,5 +1,7 @@
 package kr.co.goalkeeper.api.log;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
@@ -10,14 +12,27 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
 
 @Component
+@Slf4j
 public class CustomServletWrappingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         ContentCachingRequestWrapper wrappingRequest = new ContentCachingRequestWrapper(request);
         ContentCachingResponseWrapper wrappingResponse = new ContentCachingResponseWrapper(response);
+
         filterChain.doFilter(wrappingRequest, wrappingResponse);
+        String uuid = (String)wrappingRequest.getAttribute("uuid");
+        String type="Response ===> uuid = ";
+        log.info("{}{} {}",type,uuid,"headers start");
+        Collection<String> headerNames = wrappingResponse.getHeaderNames();
+        for (String headerName : headerNames) {
+            String header = wrappingResponse.getHeader(headerName);
+            log.info("{}{} {}: {}",type,uuid,headerName,header);
+        }
+        log.info("{}{} {}",type,uuid,"headers end");
         wrappingResponse.copyBodyToResponse();
     }
 }
