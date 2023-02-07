d5dfce4acc8827ba9a86e7c4eae8d922ac14e059
robinjoon <robin980108@naver.com>
2023-02-08 02:08:41 +0900

    테스트 코드 추가

--- src/test/java/kr/co/goalkeeper/api/GoalkeeperApplicationTests.java
+++ src/test/java/kr/co/goalkeeper/api/GoalkeeperApplicationTests.java
@@ -1,12 +1,13 @@
 package kr.co.goalkeeper.api;
 
-import kr.co.goalkeeper.api.model.entity.Certification;
-import kr.co.goalkeeper.api.model.entity.OneTimeCertification;
-import kr.co.goalkeeper.api.model.entity.User;
-import kr.co.goalkeeper.api.model.entity.Verification;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import kr.co.goalkeeper.api.model.entity.*;
+import kr.co.goalkeeper.api.model.request.ManyTimeGoalRequest;
+import kr.co.goalkeeper.api.model.response.ManyTimeGoalResponse;
 import kr.co.goalkeeper.api.repository.*;
-import kr.co.goalkeeper.api.service.OneTimeCertificationService;
-import kr.co.goalkeeper.api.service.VerificationService;
+import kr.co.goalkeeper.api.service.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,16 +31,41 @@ class GoalkeeperApplicationTests {
 	private VerificationRepository verificationRepository;
 	@Autowired
 	OneTimeCertificationRepository oneTimeCertificationRepository;
+	@Autowired
+	GoalKeeperTokenService goalKeeperTokenService;
+	@Autowired
+	UserService userService;
+	@Autowired
+	ManyTimeGoalService manyTimeGoalService;
 
 	@Test
 	@Transactional
-	@Rollback(value = false)
 	void contextLoads() {
-//		OneTimeCertification certification = oneTimeCertificationService.getCertificationByGoalId(3);
-//		User user = userRepository.findById(2L).get();
-//		Verification verification = new Verification(certification,user,true);
-//		//oneTimeCertificationRepository.save(certification);
-//		verificationRepository.save(verification);
+		OneTimeCertification certification = oneTimeCertificationService.getCertificationByGoalId(3);
+		User user = userRepository.findById(2L).get();
+		Verification verification = new Verification(certification,user,true);
+		verificationRepository.save(verification);
+	}
+	@Test
+	@Transactional
+	void test() throws JsonProcessingException {
+		long userId = goalKeeperTokenService.getUserId("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhY2Nlc3MiLCJ1c2VySUQiOjEsImV4cCI6MTY5MDUzMzA4OCwiaWF0IjoxNjc0OTgxMDg4LCJpc3MiOiJnb2FsS2VlcGVyMiJ9.hLshyCtb0FJRkdwwwYP6zJ_IEtKORinjZQTSnrJJAe4");
+		User user = userService.getUserById(userId);
+		ManyTimeGoalRequest manyTimeGoalRequest = new ObjectMapper().registerModule(new JavaTimeModule()).readValue("{\n" +
+				"  \"startDate\": \"2023-02-08\",\n" +
+				"  \"endDate\": \"2023-02-13\",\n" +
+				"  \"certDates\": [\n" +
+				"    \"2023-02-08\",\"2023-02-11\",\"2023-02-12\",\"2023-02-13\"\n" +
+				"  ],\n" +
+				"  \"title\": \"dd\",\n" +
+				"  \"categoryType\": \"STUDY\",\n" +
+				"  \"content\": \"dd\",\n" +
+				"  \"point\": 100,\n" +
+				"  \"reward\": \"HIGH_RETURN\"\n" +
+				"}",ManyTimeGoalRequest.class);
+		ManyTimeGoal manyTimeGoal = new ManyTimeGoal(manyTimeGoalRequest,user);
+		manyTimeGoal = manyTimeGoalService.createManyTimeGoal(manyTimeGoal);
+		ManyTimeGoalResponse result = new ManyTimeGoalResponse(manyTimeGoal);
 	}
 
 }
