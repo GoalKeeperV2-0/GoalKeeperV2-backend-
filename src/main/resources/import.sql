d6bc8781622ec24b03d117f3cbeb3cbd3c41ce9d
robinjoon <robin980108@naver.com>
2023-02-25 21:19:00 +0900

    테스트용 초기 데이터 분리

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,3 @@
 insert into category values ('STUDY'),('ETC'),('EXERCISE'),('HABIT'),('HOBBY');
 INSERT INTO user VALUES (1,80,'jeongye0001@gmail.com',true,'정예원','','https://lh3.googleusercontent.com/a/AEdFTp5XFsygrXFX1pHKKmHFsNo6ka6BpFL66FHCIGgf=s96-c',10000000,'MAN'),(2,null,'0108dlatnqls@gmail.com',false,'robin','','https://lh3.googleusercontent.com/a/AEdFTp7rC-sC7IRp7wWKk11kma2sT-r1140BlSHnK0Ww=s96-c',500,null);
 INSERT INTO user_category_point VALUES ('ETC',1,0),('ETC',2,0),('EXERCISE',1,0),('EXERCISE',2,0),('HABIT',1,0),('HABIT',2,0),('HOBBY',1,0),('HOBBY',2,0),('STUDY',1,0),('STUDY',2,0);
-insert into goal (dtype, id, content, goal_state, point, reward, title, category, user_id,end_date, start_date,hold_request_able) values  ('OneTimeGoal', 1, 'test', 'ONGOING', 100, 'HIGH_RETURN', 'test', 'STUDY', 1,'2023-03-13', '2023-03-09',false),('ManyTimeGoal', 2, 'dd', 'ONGOING', 100, 'HIGH_RETURN', 'dd', 'STUDY', 1,'2023-03-13', '2023-03-09',false);
-insert into one_time_goal ( id) values  (1);
-insert into many_time_goal (success_count,fail_count, id) values  (0, 0, 2);
-insert into many_time_goal_cert_date (id, cert_date, goal_id) values  (1, '2023-03-09', 2),(2, '2023-03-11', 2),(3, '2023-03-12', 2),(4, '2023-03-13', 2);
-insert into certification (dtype, id, content, date, fail_count, picture, state, success_count, goal_id)values  ('OneTimeCertification', 1, 'ddd', '2023-03-11', 3, 'dddddd', 'ONGOING', 0, 1),('ManyTimeCertification', 2, 'ddd', '2023-03-09', 0, 'dddddd', 'ONGOING', 0, 2);
-insert into one_time_certification (id) values  (1);
-insert into many_time_certification (id) values  (2);
