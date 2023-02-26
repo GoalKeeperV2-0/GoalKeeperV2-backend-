f2edff5bba7d396d3c18ebc846d0c41bf7f898f1
robinjoon <robin980108@naver.com>
2023-02-26 17:53:58 +0900

    기본 데이터에서 비밀번호 삭제

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 insert into category values ('STUDY'),('ETC'),('EXERCISE'),('HABIT'),('HOBBY');
-INSERT INTO user VALUES (1,80,'jeongye0001@gmail.com',true,'정예원','','https://lh3.googleusercontent.com/a/AEdFTp5XFsygrXFX1pHKKmHFsNo6ka6BpFL66FHCIGgf=s96-c',10000000,'MAN'),(2,null,'0108dlatnqls@gmail.com',false,'robin','','https://lh3.googleusercontent.com/a/AEdFTp7rC-sC7IRp7wWKk11kma2sT-r1140BlSHnK0Ww=s96-c',500,null);
+INSERT INTO user VALUES (1,80,'jeongye0001@gmail.com',true,'정예원','https://lh3.googleusercontent.com/a/AEdFTp5XFsygrXFX1pHKKmHFsNo6ka6BpFL66FHCIGgf=s96-c',10000000,'MAN'),(2,null,'0108dlatnqls@gmail.com',false,'robin','https://lh3.googleusercontent.com/a/AEdFTp7rC-sC7IRp7wWKk11kma2sT-r1140BlSHnK0Ww=s96-c',500,null);
 INSERT INTO user_category_point VALUES ('ETC',1,0),('ETC',2,0),('EXERCISE',1,0),('EXERCISE',2,0),('HABIT',1,0),('HABIT',2,0),('HOBBY',1,0),('HOBBY',2,0),('STUDY',1,0),('STUDY',2,0);
