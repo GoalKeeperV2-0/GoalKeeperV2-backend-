1383d09dbf17e53470fac656e9985c320706337c
robinjoon <robin980108@naver.com>
2023-03-11 18:55:25 +0900

    Refactor(ExceptionHandler) : 예외 발생 시 예외 스택 트레이스를 무조건 로깅하도록 변경

--- src/main/java/kr/co/goalkeeper/api/exception/GoalKeeperExceptionHandler.java
+++ src/main/java/kr/co/goalkeeper/api/exception/GoalKeeperExceptionHandler.java
@@ -6,18 +6,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 @Slf4j
 @RestControllerAdvice
 public class GoalKeeperExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorMessage> handle(RuntimeException e){
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        log.error("\n"+sw);
         if(e instanceof GoalkeeperException){
             GoalkeeperException exception = (GoalkeeperException) e;
             ErrorMessage errorMessage = exception.getErrorMessage();
             return ResponseEntity.status(errorMessage.getCode()).body(errorMessage);
         }else {
-            e.printStackTrace();
-            log.error(e.toString());
             ErrorMessage errorMessage = new ErrorMessage(500,"알수 없는 이유로 요청이 정상처리 되지 않았습니다.");
             return ResponseEntity.status(errorMessage.getCode()).body(errorMessage);
         }
