5170b6b0160e706c05065901dc320a504c224afc
robinjoon <robin980108@naver.com>
2023-03-12 23:17:43 +0900

    Fix(Certification) : 인증 조회시 인증의 상태가 아니라 목표의 상태를 기준으로 조회하는 버그 수정

--- src/main/java/kr/co/goalkeeper/api/repository/CertificationRepository.java
+++ src/main/java/kr/co/goalkeeper/api/repository/CertificationRepository.java
@@ -2,18 +2,16 @@ package kr.co.goalkeeper.api.repository;
 
 import kr.co.goalkeeper.api.model.entity.CategoryType;
 import kr.co.goalkeeper.api.model.entity.Certification;
-import kr.co.goalkeeper.api.model.entity.GoalState;
+import kr.co.goalkeeper.api.model.entity.CertificationState;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public interface CertificationRepository extends JpaRepository<Certification,Long> {
-    List<Certification> findAllByGoal_Category_CategoryType(CategoryType categoryType);
     Page<Certification> findAllByGoal_Id(long goalId,Pageable pageable);
     boolean existsByDateAndGoal_Id(LocalDate date,long goalId);
-    Page<Certification> findByGoal_Category_CategoryTypeAndGoal_GoalState(CategoryType categoryType, GoalState goalState, Pageable pageable);
-    Page<Certification> findByGoal_GoalState(GoalState goalState, Pageable pageable);
+    Page<Certification> findByGoal_Category_CategoryTypeAndState(CategoryType categoryType, CertificationState certificationState, Pageable pageable);
+    Page<Certification> findByState(CertificationState certificationState, Pageable pageable);
 }
