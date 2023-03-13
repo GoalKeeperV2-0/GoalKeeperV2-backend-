b3a0de524e5275690d32a359b894b9c6bed40e41
robinjoon <robin980108@naver.com>
2023-03-13 23:17:24 +0900

    Test(VerificationRequest): 테스트 객체 생성하는 코드 작성

--- src/main/java/kr/co/goalkeeper/api/model/request/VerificationRequest.java
+++ src/main/java/kr/co/goalkeeper/api/model/request/VerificationRequest.java
@@ -1,10 +1,17 @@
 package kr.co.goalkeeper.api.model.request;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class VerificationRequest {
+    public static VerificationRequest getTestInstance(long certificationId, boolean state){
+        return new VerificationRequest(certificationId,state);
+    }
     private long certificationId;
-    private long userId;
     private boolean state;
 }
