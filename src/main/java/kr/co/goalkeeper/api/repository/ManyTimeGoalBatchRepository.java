51f31c270c7169be7dfca043468a075dc791e3cd
robinjoon <robin980108@naver.com>
2023-03-14 23:06:50 +0900

    Feat(ManyTimeGoalBatchRepository) : 특정 날짜를 인증날로 가지는 ManyTimeGoal 조회 메서드 추가

--- src/main/java/kr/co/goalkeeper/api/repository/ManyTimeGoalBatchRepository.java
+++ src/main/java/kr/co/goalkeeper/api/repository/ManyTimeGoalBatchRepository.java
@@ -5,10 +5,13 @@ import kr.co.goalkeeper.api.model.entity.ManyTimeGoal;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 
 public interface ManyTimeGoalBatchRepository extends JpaRepository<ManyTimeGoal,Long> {
     Slice<ManyTimeGoal> findAllByGoalStateAndEndDate(GoalState goalState, LocalDate date, Pageable pageable);
-
+    @Query(value = "select * from Goal g join (select goal_id from many_time_goal_cert_date mtgcd where mtgcd.cert_date = :localDate) as res on res.goal_id = g.id " +
+            "join many_time_goal mtg on mtg.id = g.id",nativeQuery = true,countQuery = "select count(*) from Goal g join (select goal_id from many_time_goal_cert_date mtgcd where mtgcd.cert_date = :localDate) as res on res.goal_id = g.id join many_time_goal mtg on mtg.id = g.id")
+    Slice<ManyTimeGoal> findAllByCertDatesContainingAndGoalState(LocalDate localDate,Pageable pageable);
 }
