ca6d369002c452c9df965954f78cfe32a28875a3
robinjoon <robin980108@naver.com>
2023-03-15 10:50:51 +0900

    Feat(NotificationSender) : DDay 알림 타입 생성

--- src/main/java/kr/co/goalkeeper/api/model/entity/NotificationType.java
+++ src/main/java/kr/co/goalkeeper/api/model/entity/NotificationType.java
@@ -1,5 +1,5 @@
 package kr.co.goalkeeper.api.model.entity;
 
 public enum NotificationType {
-    GOAL_ADD,CERT_ADD,GOAL_RESULT,CERT_RESULT,HOLD_REQUEST,HOLD_RESULT
+    GOAL_ADD,CERT_ADD,GOAL_RESULT,CERT_RESULT,HOLD_REQUEST,HOLD_RESULT,D_DAY
 }
