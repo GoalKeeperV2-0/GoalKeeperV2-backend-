1d741a6a4bd633aab876c1963c2f44406183c10a
robinjoon <robin980108@naver.com>
2023-03-15 10:50:36 +0900

    Feat(NotificationSender) : 알림 한번에 여러개 보내는 메서드 추가

--- src/main/java/kr/co/goalkeeper/api/service/port/NotificationService.java
+++ src/main/java/kr/co/goalkeeper/api/service/port/NotificationService.java
@@ -4,7 +4,10 @@ import kr.co.goalkeeper.api.model.entity.Notification;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
+import java.util.List;
+
 public interface NotificationService {
     void sendNotification(Notification notification);
+    void sendNotification(List<Notification> notifications);
     Slice<Notification> getNotifications(long userId, Pageable pageable);
 }
