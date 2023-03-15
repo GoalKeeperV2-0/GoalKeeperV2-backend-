57e3250337a32355210541687cc202767f315df4
robinjoon <robin980108@naver.com>
2023-03-15 10:50:31 +0900

    Feat(NotificationSender) : 알림 한번에 여러개 보내는 메서드 추가

--- src/main/java/kr/co/goalkeeper/api/NotificationSender.java
+++ src/main/java/kr/co/goalkeeper/api/NotificationSender.java
@@ -4,6 +4,8 @@ import kr.co.goalkeeper.api.model.entity.Notification;
 import kr.co.goalkeeper.api.service.port.NotificationService;
 import org.springframework.context.ApplicationContext;
 
+import java.util.List;
+
 public class NotificationSender {
     private static ApplicationContext applicationContext;
     static void init(ApplicationContext applicationContext){
@@ -12,5 +14,9 @@ public class NotificationSender {
     public static void send(Notification notification){
         NotificationService notificationService = applicationContext.getBean(NotificationService.class);
         notificationService.sendNotification(notification);
+    }
+    public static void send(List<Notification> notifications){
+        NotificationService notificationService = applicationContext.getBean(NotificationService.class);
+        notificationService.sendNotification(notifications);
     }
 }
