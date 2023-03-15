2bac69b3b0ec291f0952e97534881ac40e6d3561
robinjoon <robin980108@naver.com>
2023-03-15 10:49:32 +0900

    Refactor(ManyTimeGoalBatchRepository) : sql 수정

--- src/main/java/kr/co/goalkeeper/api/repository/ManyTimeGoalBatchRepository.java
+++ src/main/java/kr/co/goalkeeper/api/repository/ManyTimeGoalBatchRepository.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 
 public interface ManyTimeGoalBatchRepository extends JpaRepository<ManyTimeGoal,Long> {
     Slice<ManyTimeGoal> findAllByGoalStateAndEndDate(GoalState goalState, LocalDate date, Pageable pageable);
-    @Query(value = "select * from Goal g join (select goal_id from many_time_goal_cert_date mtgcd where mtgcd.cert_date = :localDate) as res on res.goal_id = g.id " +
-            "join many_time_goal mtg on mtg.id = g.id",nativeQuery = true,countQuery = "select count(*) from Goal g join (select goal_id from many_time_goal_cert_date mtgcd where mtgcd.cert_date = :localDate) as res on res.goal_id = g.id join many_time_goal mtg on mtg.id = g.id")
-    Slice<ManyTimeGoal> findAllByCertDatesContainingAndGoalState(LocalDate localDate,Pageable pageable);
+    @Query(value = "select * from Goal g join (select goal_id from many_time_goal_cert_date mtgcd where mtgcd.cert_date = CAST(:localDate as DATE)) as res on res.goal_id = g.id " +
+            "join many_time_goal mtg on mtg.id = g.id where g.goal_state='ONGOING'",nativeQuery = true,countQuery = "select count(*) from Goal g join (select goal_id from many_time_goal_cert_date mtgcd where mtgcd.cert_date = :localDate) as res on res.goal_id = g.id join many_time_goal mtg on mtg.id = g.id")
+    Slice<ManyTimeGoal> findAllByCertDatesContaining(String localDate,Pageable pageable);
 }
