6f9b099345eb1dad9ef8333341129cd2d6fdedc2
robinjoon <robin980108@naver.com>
2023-03-20 01:34:17 +0900

    Fix(BatchRepository): N + 1문제 해결

--- src/main/java/kr/co/goalkeeper/api/repository/GoalBatchRepository.java
+++ src/main/java/kr/co/goalkeeper/api/repository/GoalBatchRepository.java
@@ -4,10 +4,12 @@ import kr.co.goalkeeper.api.model.entity.Goal;
 import kr.co.goalkeeper.api.model.entity.GoalState;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 
 public interface GoalBatchRepository extends JpaRepository<Goal,Long> {
+    @EntityGraph(attributePaths = {"user","category"})
     Slice<Goal> findAllByEndDateAndGoalState(LocalDate date, GoalState goalState, Pageable pageable);
 }
--- src/main/java/kr/co/goalkeeper/api/repository/OneTimeGoalBatchRepository.java
+++ src/main/java/kr/co/goalkeeper/api/repository/OneTimeGoalBatchRepository.java
@@ -4,10 +4,12 @@ import kr.co.goalkeeper.api.model.entity.GoalState;
 import kr.co.goalkeeper.api.model.entity.OneTimeGoal;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 
 public interface OneTimeGoalBatchRepository extends JpaRepository<OneTimeGoal, Long> {
+    @EntityGraph(attributePaths = {"user","category"})
     Slice<OneTimeGoal> findAllByGoalStateAndEndDate(GoalState goalState, LocalDate date, Pageable pageable);
 }
