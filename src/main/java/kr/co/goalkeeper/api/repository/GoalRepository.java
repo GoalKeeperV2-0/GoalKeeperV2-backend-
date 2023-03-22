9c8785bfa219e61929958e5ddcfacc9306bc5f81
robinjoon <robin980108@naver.com>
2023-03-22 23:21:29 +0900

    Fix(GoalRepository): findGoalIdsByUSerIdAndNeedCertNow 메서드 쿼리 user_id 고정값 오타 수정

--- src/main/java/kr/co/goalkeeper/api/repository/GoalRepository.java
+++ src/main/java/kr/co/goalkeeper/api/repository/GoalRepository.java
@@ -18,7 +18,7 @@ public interface GoalRepository extends JpaRepository<Goal,Long> {
     long countAllByGoalState(GoalState goalState);
     long countAllByUser_Id(long user_id);
     long countAllByGoalStateAndUser_Id(GoalState goalState,long userId);
-    @Query(value = "select id from((select g.id as id,g.start_date from goal g where g.end_date = CAST(:now as DATE) and g.id not in (select c.goal_id from certification c where c.date = CAST(:now as DATE)) and g.user_id =:userId order by g.start_date limit 2)union(select g.id as id,g.start_date from goal g join many_time_goal mtg on g.id = mtg.id join many_time_goal_cert_date mtgcd on g.id = mtgcd.goal_id where mtgcd.cert_date = CAST(:now as DATE) and g.user_id =1 and g.id not in (select c.goal_id from certification c where c.date = CAST(:now as DATE))order by g.start_date )order by start_date) as isdisd order by start_date limit 2",nativeQuery = true)
+    @Query(value = "select id from((select g.id as id,g.start_date from goal g where g.end_date = CAST(:now as DATE) and g.id not in (select c.goal_id from certification c where c.date = CAST(:now as DATE)) and g.user_id =:userId order by g.start_date limit 2)union(select g.id as id,g.start_date from goal g join many_time_goal mtg on g.id = mtg.id join many_time_goal_cert_date mtgcd on g.id = mtgcd.goal_id where mtgcd.cert_date = CAST(:now as DATE) and g.user_id =:userId and g.id not in (select c.goal_id from certification c where c.date = CAST(:now as DATE))order by g.start_date )order by start_date) as isdisd order by start_date limit 2",nativeQuery = true)
     List<Long> findGoalIdsByUSerIdAndNeedCertNow(LocalDate now, long userId);
     @EntityGraph(attributePaths = "category")
     @Override
